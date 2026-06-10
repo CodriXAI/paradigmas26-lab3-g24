a00fa96935e6ab56cf982c4c3e49910fb8c0ecdc
CodriXAI <codrix.ai@gmail.com>
2026-06-10 16:04:56 -0300

    fix: Main limpio hasta el ejercicio 2b para posteriores implementaciones

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -38,7 +38,7 @@ object Main {
     // Download feeds and parse posts, tracking success/failure
     val allPostsRDD = subscriptionsRDD.flatMap { subscription =>
       try{
-        FileIO.downloadFeed(subscription.url) match {
+        FileIO.downloadFeed(subscription) match {
           case None =>
             // In case of a download error, a warning is logged and an empty list of posts 
             // for that subscription is returned.
@@ -60,61 +60,7 @@ object Main {
     val filteredPostsRDD = allPostsRDD.filter(post =>
       post.title.nonEmpty && post.selftext.nonEmpty
     )
-
-    // Count feed successes/failures
-    val feedsSuccess = downloadResults.count(_._1)
-    val feedsFailed = downloadResults.length - feedsSuccess
-
-    // Flatten all posts and count JSON parse failures
-    val allPosts = downloadResults.flatMap(_._2)
-    val postsSuccess = allPosts.length
-    val postsFailed = downloadResults.count(_._2.isEmpty)
-
-    // Filter empty posts
-    val filteredPosts = Analyzer.filterEmptyPosts(allPosts)
-    val postsFiltered = allPosts.length - filteredPosts.length
-
-    // Calculate average characters in filtered posts
-    val totalChars = filteredPosts.map(post => post.title.length + post.selftext.length).sum
-    val avgChars = if (filteredPosts.nonEmpty) totalChars / filteredPosts.length else 0
-
-    // Prepare statistics
-    val stats = Map(
-      "feedsSuccess" -> feedsSuccess,
-      "feedsFailed" -> feedsFailed,
-      "postsSuccess" -> postsSuccess,
-      "postsFailed" -> postsFailed,
-      "postsFiltered" -> postsFiltered,
-      "avgChars" -> avgChars
-    )
-
-    // Print output
-    println(Formatters.formatProcessingStats(stats))
-    println()
-
-    // Check if we have any posts to process
-    if (filteredPosts.isEmpty) {
-      println("Error: No valid posts downloaded after filtering")
-      return
-    }
-
-    // Load dictionaries
-    val dictionary = Dictionary.loadAll(cmdArgs.entitiesDir)
-
-    // Detect entities in all posts (combine title and selftext)
-    val allEntities = filteredPosts.flatMap { post =>
-      val combinedText = post.title + " " + post.selftext
-      Analyzer.detectEntities(combinedText, dictionary)
-    }
-
-    // Count entities
-    val entityCounts = Analyzer.countEntities(allEntities)
-    val typeStats = Analyzer.countByType(allEntities)
-
-    println(Formatters.formatTypeStats(typeStats))
-    println()
-    println(Formatters.formatEntityStats(entityCounts, cmdArgs.topK))
-
+    
     spark.stop()
   }
 }
