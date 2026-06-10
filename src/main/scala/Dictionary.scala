c557a78ced834a2015c87bc76a243f8dd2ddda92
Fernando Ampuero <fernando.ampuero@mi.unc.edu.ar>
2026-06-10 17:16:51 -0300

    feat: Agrego manejo de casos de error en dictionary

--- src/main/scala/Dictionary.scala
+++ src/main/scala/Dictionary.scala
@@ -32,15 +32,34 @@ object Dictionary {
     // Check if entities directory exists
     val dataDir = new java.io.File(entitiesDir)
 
+    if (!dataDir.exists()) {
+      println(s"Error: entities directory '${entitiesDir}' not found")
+    }
+
     val peopleOpt = loadFromFile(s"$entitiesDir/people.txt", "Person")
+    if (peopleOpt.isEmpty) {
+      println(s"Warning: Could not load $entitiesDir/people.txt")
+    }
 
     val universitiesOpt = loadFromFile(s"$entitiesDir/universities.txt", "University")
+    if (universitiesOpt.isEmpty) {
+      println(s"Warning: Could not load $entitiesDir/universities.txt")
+    }
 
     val languagesOpt = loadFromFile(s"$entitiesDir/languages.txt", "ProgrammingLanguage")
+    if (languagesOpt.isEmpty) {
+      println(s"Warning: Could not load $entitiesDir/languages.txt")
+    }
 
     val organizationsOpt = loadFromFile(s"$entitiesDir/organizations.txt", "Organization")
+    if (organizationsOpt.isEmpty) {
+      println(s"Warning: Could not load $entitiesDir/organizations.txt")
+    }
 
     val placesOpt = loadFromFile(s"$entitiesDir/places.txt", "Place")
+    if (placesOpt.isEmpty) {
+      println(s"Warning: Could not load $entitiesDir/places.txt")
+    }
 
     peopleOpt.getOrElse(List()) :::
       universitiesOpt.getOrElse(List()) :::
